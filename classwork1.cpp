644dda961624509f9fcedb19edc799b4f959b4b1
DengDeng4 <majier.deng@strathmore.edu>
2024-10-15 20:22:26 +0300

    class work 2

--- classwork1.cpp
+++ classwork1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Strathmore Is a good university" << std::endl;
+    cout << "        O     O      " << endl;
+    cout << "       /|\\   /|\\     " << endl;
+    cout << "      / | \\ / | \\    " << endl;
+    cout << "        |     |      " << endl;
+    cout << "       / \\   / \\     " << endl;
+    cout << "      /   \\ /   \\    " << endl;
+    cout << "------------------------" << endl;
+    cout << "    __        ____ " << endl;
+    cout << "   /    \\__/        \\" << endl;
+    cout << "  /      |    |          \\" << endl;
+    cout << " /__|_|_____\\ " << endl;
+    cout << " |________|" << endl;
+    cout << "         0               0   ";
     return 0;
 }
