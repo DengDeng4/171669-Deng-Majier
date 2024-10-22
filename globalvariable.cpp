e2a97ad8fba5e648dcf7db7275b578a8f0cc1515
DengDeng4 <majier.deng@strathmore.edu>
2024-10-22 18:31:04 +0300

    global variable

--- globalvariable.cpp
+++ globalvariable.cpp
@@ -1,1 +1,19 @@
-#include
+#include <iostream>
+using namespace std;
+
+int globalVar = 300;
+const int age = 500;
+
+void myFunction()
+{
+    // Function code Block
+
+}
+int main(){
+    // Function Code Block
+    int age = 30;
+    cout <<age<<endl;
+
+    return 0;
+}
+
