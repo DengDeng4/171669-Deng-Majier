383c016c537efd58479c50a5a41d8f807b9ca670
[DengDeng4] <[majier.deng@strathmore.edu]>
2024-10-29 20:02:52 +0300

    Area 2

--- classwork2.cpp
+++ classwork2.cpp
@@ -3,16 +3,9 @@ using namespace std;
 
 int main(){
     //Data type and variable declaration
-int length ;
-int width ;
- double area ;
- //Length Input
- cout<< "Insert the length of the rectangle";
- cin>> length;
- //Width input
- cout<<"Insert width of the rectangle";
- cin>> width;
- //calculation of area
+int length = 5 ;
+int width = 8 ;
+double area;
  area = length*width;
  
 cout<<" The area of the rectangle is"<< area << endl;
