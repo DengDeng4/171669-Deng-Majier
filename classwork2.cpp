3317f2555368f1487ca1b64383663c249328a743
[DengDeng4] <[majier.deng@strathmore.edu]>
2024-10-29 19:20:41 +0300

    Area of a rectangle

---
+++ classwork2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Data type and variable declaration
+int length ;
+int width ;
+ double area ;
+ //Length Input
+ cout<< "Insert the length of the rectangle";
+ cin>> length;
+ //Width input
+ cout<<"Insert width of the rectangle";
+ cin>> width;
+ //calculation of area
+ area = length*width;
+ 
+cout<<" The area of the rectangle is"<< area << endl;
+return 0;
+}
