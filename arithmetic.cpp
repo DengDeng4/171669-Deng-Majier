79b15c0fc5542b42ab53b0ee273187495a666b15
[DengDeng4] <[majier.deng@strathmore.edu]>
2024-11-05 18:55:47 +0300

    arithmetic 1

---
+++ arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int value1, value2;
+
+    cout << "enter the first number";
+    cin >> value1;
+
+    cout << "enter the second number";
+    cin >> value2;
+
+    cout << "the sum of the two numbers is: " << value1 + value2 << endl;
+
+    return 0;
+}
